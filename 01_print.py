36b540487473e102841f2d748a02aef5bb03ef20
Blanca Itzel <itzelgarciaruiz@outlook.com>
2024-05-05 16:33:45 -0600

    Hola mundo

---
+++ 01_print.py
@@ -0,0 +1,16 @@
+print("Hola, esto es archivo 01")
+
+print("Hola soy Itzel y tengo 26 años")
+
+# operaciones
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+#Esto es un comentario
+
+"""
+comentario de varias lineas
+
+"""
