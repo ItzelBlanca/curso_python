bc30eb573b62e7b60cc37e4d68beffaf43c34be1
Blanca Itzel <itzelgarciaruiz@outlook.com>
2024-05-05 16:34:48 -0600

    Variables

---
+++ 02_vars.py
@@ -0,0 +1,18 @@
+print("Hola, vars")
+
+#Esto es una variable
+my_name = "Blanca"
+print(my_name)
+
+#Esto es una variable con un numero
+my_age = 12
+print(my_age)
+
+my_name = "Itzel"
+print("aqui cambio ",my_name)
+
+#input
+
+my_name = input("¿Cual es tu nombre?")
+my_age = input("¿Cual es tu edad? ")
+print("Usando input ", my_name,"y tienes ", my_age, "años")
