02a984754a09f9562978c4f19aeef1e8cdc512bf
Blanca Itzel <itzelgarciaruiz@outlook.com>
2024-05-07 18:41:41 -0600

    Arith

---
+++ 08_arith.py
@@ -0,0 +1,17 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2) #5
+print(10 % 2) #0
+print(10 / 3)
+print(10 % 3) #residuo
+print(10 // 3) #solo nos quedamos con el numero entero de una divicion
+print(2 ** 3) #exponencial
+print(2 ** 3 + 3 - 7 / 1 // 4) #PEMDAS
+print(2 ** 3)
+print((7 / 1) // 4)
+print( 8 + 3 - 1 )
+
+
+print('Hola' + 'mundo')
+print('Hola'*3)
