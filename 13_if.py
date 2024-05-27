7053d5775a8e27b1c19019f77033d40eef52ba80
Blanca Itzel <itzelgarciaruiz@outlook.com>
2024-05-27 11:52:04 -0600

    If

---
+++ 13_if.py
@@ -0,0 +1,29 @@
+if True:
+    print("deberia ejecutarse")
+
+
+if False:
+    print("Nunca se ejecuta")  
+      
+
+pet = input("¿Cuales tu mascota favorita?")
+
+if pet == "perro":
+    print("genial tienes buen gusto")
+    
+elif pet == "gato":
+    print("eres muy inteligente")
+elif pet == "pez":
+    print("Eres lo maximo")
+else:
+    print("no tienes ninguna mascota interesante")    
+    
+'''
+stock = int(input("Digita el stock =>"))
+
+if stock >= 100 and stock <= 1000:
+    print("El stock es correcto")
+else:
+    print("El stock es incorreco")    
+'''
+   
