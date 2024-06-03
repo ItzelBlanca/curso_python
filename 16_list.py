05cb667c885ed8881c00291209c2dc807ed66852
Blanca Itzel <itzelgarciaruiz@outlook.com>
2024-06-03 12:34:27 -0600

    List

---
+++ 16_list.py
@@ -0,0 +1,24 @@
+numers = [1, 2, 3, 4]
+print(numers)
+print(type(numers))
+
+tasks = ['make a dishes', 'play videogames']
+print(tasks)
+
+types = [1, True, 'Hola']
+print(types)
+
+print(numers[0])
+print(tasks[0])
+
+text = 'Hola'
+#text[0] = 'w'
+
+tasks[0] = 'watch platzi courses'
+print(tasks)
+tasks[0] = 'do the dishes'
+print(tasks)
+
+print(numers[:3])
+print(True in types)
+print('Hola' in types)
